95e0495c754934ffe0deda7f6d76e591f00ce05a
Wojciech T. Borkowski <wborkowsk@gmail.com>
2020-12-18 17:24:36 +0100

    Przemianowanie_alg_na_pas

---
+++ simple/ALGO/gitmv.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+git mv	'ABCTablic.alg.pas'		ABCTablic.pas
+git mv	'bitowe.alg'		        bitowe.pas
+git mv	'czytajplikpoznaku.alg'		czytajplikpoznaku.pas
+git mv	'czytaliniepliku.alg'		czytaliniepliku.pas
+git mv	'deklaracja_string.alg'		deklaracja_string.pas
+git mv	'dodawanie_jedynki.alg'		dodawanie_jedynki.pas
+git mv	'DoWyboru2.alg'		        DoWyboru2.pas
+git mv	'DoWyboru.alg.pas'		DoWyboru.pas
+git mv	'dziel.alg'		        dziel.pas
+git mv	'dzielcalkowite.alg'		dzielcalkowite.pas
+git mv	'DziwnePrzypisania.alg'		DziwnePrzypisania.pas
+git mv	'FirstLine.alg'		        FirstLine.pas
+git mv	'FunkcjeMat.alg'		FunkcjeMat.pas
+git mv	'Gierka.alg.pas'		Gierka.pas
+git mv	'Gierka KOPIA.alg'		Gierka_KOPIA.pas
+git mv	'gitmv.sh'		        gitmv.sh
+git mv	'grafika.alg'		        grafika.pas
+git mv	'IEEE.alg'		        IEEE.pas
+git mv	'IEEE_test.alg'		        IEEE_test.pas
+git mv	'IEEv2.alg'		        IEEv2.pas
+git mv	'interface_graf_ALGO.alg'	interface_graf_ALGO.pas
+git mv	'jeżeli2.alg'		        jeżeli2.pas
+git mv	'jeżeli.alg'		        jeżeli.pas
+git mv	'kalkulator.alg'		kalkulator.pas
+git mv	'kolorowekola_v1.alg'		kolorowekola_v1.pas
+git mv	'kolorowekola_v2.alg'		kolorowekola_v2.pas
+git mv	'koloryRGB.alg.pas'		koloryRGB.pas
+git mv	'konkatenacja.alg'		konkatenacja.pas
+git mv	'modul.alg'		        modul.pas
+git mv	'modulo.alg'		        modulo.pas
+git mv	'modul_v0.alg'		        modul_v0.pas
+git mv	'modul_v1.alg'		        modul_v1.pas
+git mv	'okno.alg'		        okno.pas
+git mv	'param_tabl.alg'		param_tabl.pas
+git mv	'param_tabl_los.alg'		param_tabl_los.pas
+git mv	'param_tabl.pas.alg'		param_tabl.pas
+git mv	'PawieOczko.alg'		PawieOczko.pas
+git mv	'petlaFor.alg'		        petlaFor.pas
+git mv	'PetlaRepeat.alg'		PetlaRepeat.pas
+git mv	'PetlaWhile.alg'		PetlaWhile.pas
+git mv	'Podprogramy.alg'		Podprogramy.pas
+git mv	'Porównania.alg'		Porównania.pas
+git mv	'przypisanie.alg'		przypisanie.pas
+git mv	'repeat_vel_skok.alg'		repeat_vel_skok.pas
+git mv	'RownKwProc.alg'		RownKwProc.pas
+git mv	'skok.alg'		        skok.pas
+git mv	'sredniaTab2.alg'		sredniaTab2.pas
+git mv	'sredniaTab2 v1.alg'		sredniaTab2_v1.pas
+git mv	'sredniaTablicy.alg'		sredniaTablicy.pas
+git mv	'sredniaTablicy v1.alg'		sredniaTablicy_v1.pas
+git mv	'stale.alg'		        stale.pas
+git mv	'suma.alg'		        suma.pas
+git mv	'sumuj.alg'		        sumuj.pas
+git mv	'tablica1_liczbypalnin.20kwa.alg'		tablica1_liczbypalnin.20kwa.pas
+git mv	'TestNapisow.alg'		TestNapisow.pas
+git mv	'testzawijania.alg'		testzawijania.pas
+git mv	'wybieranie.alg'		wybieranie.pas
+git mv	'wypisywanieTab.alg'		wypisywanieTab.pas
+git mv	'wypisywanieTab v1.alg'		wypisywanieTab_v1.pas
+git mv	'Zakres.alg'		        Zakres.pas
+git mv	'zdarzenia_demo.alg'		zdarzenia_demo.pas
+git mv	'zdarzenia_myszy.alg'		zdarzenia_myszy.pas
+git mv	'zdarzenia_myszy_graficznie.alg'		zdarzenia_myszy_graficznie.pas
+git mv	'zdarzenia_myszy_graficznie v1.alg'		zdarzenia_myszy_graficznie_v1.pas
+git mv	'zdarzenia_myszy v1.alg'	zdarzenia_myszy_v1.pas
+git mv	'ZgadnijCoTo.alg'		ZgadnijCoTo.pas
+git mv	'zmienne.alg'		        zmienne.pas
+git mv	'ZobaczANSI.alg'		ZobaczANSI.pas
+git mv	'ZobaczANSI_v2.alg.pas'		ZobaczANSI_v2.pas
+git mv	'ZrobiPlik.alg'		        ZrobiPlik.pas
+git mv	'ZrobiPlik v1.alg'		ZrobiPlik_v1.pas
